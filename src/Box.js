a576c731f568c570096cddec38cc31af740dabab
Katie Lamber <katielamber02@gmail.com>
2020-01-24 21:22:33 +0200

    box rotates and increases on hover

--- src/Box.js
+++ src/Box.js
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useSpring, a } from "react-spring/three";
-
+import { extend, useThree, useFrame } from "react-three-fiber";
 export const Box = () => {
+  const myRef = useRef();
   const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
   const props = useSpring({
     scale: active ? [1.5, 1.5, 1.5, 1.5] : [1, 1, 1, 1],
     color: hovered ? "aqua" : "grey"
   });
+  useFrame(() => {
+    myRef.current.rotation.y += 0.01;
+  });
   return (
     <a.mesh
+      ref={myRef}
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(!active)}
