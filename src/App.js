c41cd62a126d002665b75c4a83b6a350488552d2
Katie Lamber <katielamber02@gmail.com>
2020-01-24 20:42:05 +0200

    useSpring and a

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Canvas } from "react-three-fiber";
-
 import { Box } from "./Box";
 
 function App() {
--- src/Box.js
+++ src/Box.js
@@ -1,15 +1,22 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
+import { useSpring, a } from "react-spring/three";
 
 export const Box = () => {
   const [hovered, setHovered] = useState(false);
-
+  const [active, setActive] = useState(false);
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5, 1.5] : [1, 1, 1, 1],
+    color: hovered ? "aqua" : "grey"
+  });
   return (
-    <mesh
+    <a.mesh
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}
     >
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshBasicMaterial attach="material" color={hovered ? "aqua" : "grey"} />
-    </mesh>
+      <a.meshBasicMaterial attach="material" color={props.color} />
+    </a.mesh>
   );
 };
